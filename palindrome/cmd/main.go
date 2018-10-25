2a8cc33c4062cb4bd2fe4288ee0cd0764dee10b7
rreed_ext <rreed_ext@icontrol.com>
2018-10-25 07:55:58 -0500

    added CheckArgs()

--- palindrome/cmd/main.go
+++ palindrome/cmd/main.go
@@ -1,5 +1,32 @@
 package main
 
+import (
+	"errors"
+	"fmt"
+	"os"
+)
+
+var (
+	noArgumentsError      = errors.New("You must provide one string argument (with no spaces)")
+	invalidArgumentsError = errors.New("Too many arguments. Please enter a string with no spaces")
+)
+
 func main() {
 
+	// palindrome.PalindromeCheck(os.Args)
+
+}
+
+func ArgsCheck(input []string) string {
+	switch {
+	case len(input) == 0:
+		fmt.Println("no args")
+		os.Exit(1)
+	case len(input) > 1:
+		fmt.Println("too many args")
+		os.Exit(1)
+		// default:
+		// 	return input[0]
+	}
+	return input[0]
 }
